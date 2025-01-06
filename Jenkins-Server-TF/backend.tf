0b536194c73009f4c912c4e33f453b13bb06443e
Sumit Budhawant <budhawant.sumiti8@gmail.com>
2025-01-06 14:42:43 +0530

    Initial

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
-    region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
+    bucket         = "my-ews-bucket7"
+    region         = "ap-south-1"
+    key            = "Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
